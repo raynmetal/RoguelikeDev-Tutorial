31357d7987f6e32f7c7f2dde5f233a978c38d9e9
Zoheb Shujauddin <zoheb2424@gmail.com>
2023-02-14 19:45:40 +0530

    abstracts characters and objects into an entity class, moves rendering and event handling responsibilities to an engine class

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 import tcod
 
-from actions import EscapeAction, MovementAction
+from engine import Engine
 from input_handlers import EventHandler
+from entity import Entity
 
 def main():
-    screen_width = 80;
-    screen_height = 50;
+    screen_width = 80
+    screen_height = 50
 
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
     event_handler = EventHandler()
 
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255,255,255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255,255,0))
+    entities = {npc, player}
+
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
     with tcod.context.new_terminal(
         screen_width, 
         screen_height, 
@@ -25,24 +29,9 @@ def main():
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=player_x, y=player_y, string="@")
-
-            context.present(root_console)
-
-            root_console.clear()
-
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
-
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
-                
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
+            engine.render(console=root_console, context=context)
+            events = tcod.event.wait()
+            engine.handle_events(events)
 
 if __name__ == "__main__":
     main()
