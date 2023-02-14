08fefbe58f63134e261b8f637e17db1d839de72f
Zoheb Shujauddin <zoheb2424@gmail.com>
2023-02-15 00:07:08 +0530

    adds a function that generates tunnels between rooms

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # r/RoguelikeDev Roguelike Tutorial
 
-A roguelike game I will make by following the roguelike tutorial on the subreddit r/RoguelikeDev. Doing this to work up the courage to work on my own game.
+A roguelike game I'm making by following the roguelike tutorial on the subreddit r/RoguelikeDev. Doing this to work up the courage to work on my own game.
 
 [Link to the relevant wiki on the subreddit](https://www.reddit.com/r/roguelikedev/wiki/python_tutorial_series/)
--- procgen.py
+++ procgen.py
@@ -1,4 +1,7 @@
-from typing import Tuple
+import random
+from typing import Iterator, Tuple
+
+import tcod
 
 from game_map import GameMap
 import tile_types
@@ -24,6 +27,29 @@ class RectangularRoom:
         """
         return slice(self.x1 + 1, self.x2), slice(self.y1 + 1, self.y2)
 
+def tunnel_between(
+    start: Tuple[int, int], end: Tuple[int, int]
+) -> Iterator[Tuple[int, int]]:
+    """
+    Return an L-shaped tunnel between these two points.
+    """
+    x1, y1 = start,
+    x2, y2 = end
+
+    # Move horizontally, then vertically
+    if random.random() < 0.5:
+        corner_x, corner_y = x2, y1
+
+    # Vertically, then horizontally
+    else:
+        corner_x, corner_y = x1, y2
+    
+    # Generate the coordinates for this tunnel.
+    for x, y in tcod.los.bresenham((x1, y1), (corner_x, corner_y)).tolist():
+        yield x, y
+    for x, y in tcod.los.bresenham((corner_x, corner_y), (x2, y2)).tolist():
+        yield x, y
+
 def generate_dungeon(map_width: int, map_height: int) -> GameMap:
     dungeon = GameMap(map_width, map_height)
 
@@ -32,5 +58,8 @@ def generate_dungeon(map_width: int, map_height: int) -> GameMap:
 
     dungeon.tiles[room_1.inner] = tile_types.floor
     dungeon.tiles[room_2.inner] = tile_types.floor
+
+    for x, y in tunnel_between(room_2.center, room_1.center):
+        dungeon.tiles[x, y] = tile_types.floor
 
     return dungeon
