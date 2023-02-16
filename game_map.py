85bf71dc5afdaed1222d2366f021bc2fdb579c79
Zoheb Shujauddin <zoheb2424@gmail.com>
2023-02-16 17:03:31 +0530

    adds the ability to bump into enemies

--- game_map.py
+++ game_map.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Iterable, TYPE_CHECKING
+from typing import Iterable, TYPE_CHECKING, Optional
 
 import numpy as np
 from tcod.console import Console
@@ -23,6 +23,13 @@ class GameMap:
         )
         self.visible = np.full((width, height), fill_value=False, order="F")
         self.explored = np.full((width, height), fill_value=False, order="F")
+    
+    def get_blocking_entity_at_location(self, location_x: int, location_y: int) -> Optional[Entity]:
+        for entity in self.entities:
+            if entity.blocks_movement and entity.x == location_x and entity.y == location_y:
+                return entity
+
+        return None
 
     def in_bounds(self, x: int, y: int) -> bool:
         """
