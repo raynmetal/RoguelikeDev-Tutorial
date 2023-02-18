dd7ea535df707350a1b6e5d97ac679ac941bd388
Zoheb Shujauddin <zoheb2424@gmail.com>
2023-02-18 23:04:15 +0530

    implements render order and death

--- entity.py
+++ entity.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import copy
 from typing import Optional, Tuple, Type, TypeVar, TYPE_CHECKING
 
+from render_order import RenderOrder
+
 if TYPE_CHECKING:
     from components.ai import BaseAI
     from components.fighter import Fighter
@@ -24,6 +26,7 @@ class Entity:
         color: Tuple[int, int, int] = (255,255,255),
         name: str = "<Unnamed>",
         blocks_movement: bool = False,
+        render_order: RenderOrder = RenderOrder.CORPSE,
     ):
         self.x = x
         self.y = y 
@@ -31,6 +34,7 @@ class Entity:
         self.color = color
         self.name = name
         self.blocks_movement = blocks_movement
+        self.render_order = render_order
 
         # If gamemap isn't provided now, it'll be set later
         if gamemap:
@@ -84,6 +88,7 @@ class Actor(Entity):
             color=color,
             name=name,
             blocks_movement=True,
+            render_order=RenderOrder.ACTOR,
         )
 
         self.ai: Optional[BaseAI] = ai_cls(self)
--- input_handlers.py
+++ input_handlers.py
@@ -40,6 +40,13 @@ WAIT_KEYS = {
 class EventHandler(tcod.event.EventDispatch[Action]):
     def __init__(self, engine: Engine):
         self.engine = engine
+
+    def handle_events(self) -> None:
+        raise NotImplementedError()
+    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
+        raise SystemExit()
+
+class MainGameEventHandler(EventHandler):
     def handle_events(self) -> None:
         for event in tcod.event.wait():
             action = self.dispatch(event)
@@ -51,9 +58,6 @@ class EventHandler(tcod.event.EventDispatch[Action]):
             self.engine.handle_enemy_turns()
 
             self.engine.update_fov() # Update FOV before the player's next action
-
-    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
-        raise SystemExit()
 
     def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
         action: Optional[Action] = None
@@ -70,6 +74,27 @@ class EventHandler(tcod.event.EventDispatch[Action]):
 
         elif key == tcod.event.K_ESCAPE:
             action = EscapeAction(player)
+
+        # No valid key was pressed
+        return action
+
+class GameOverEventHandler(EventHandler):
+    def handle_events(self) -> None:
+        for event in tcod.event.wait():
+            action = self.dispatch(event)
+
+            if action is None:
+                continue
+
+            action.perform()
+
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
+        action: Optional[Action] = None
+
+        key = event.sym
+
+        if key == tcod.event.K_ESCAPE:
+            action = EscapeAction(self.engine.player)
 
         # No valid key was pressed
         return action
