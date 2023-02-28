b3421bab711243df4f6ddc3f3d71c088bf4761ac
Zoheb Shujauddin <zoheb2424@gmail.com>
2023-03-01 02:17:38 +0530

    adds equippable items

--- entity.py
+++ entity.py
@@ -9,6 +9,8 @@ from render_order import RenderOrder
 if TYPE_CHECKING:
     from components.ai import BaseAI
     from components.consumable import Consumable
+    from components.equipment import Equipment
+    from components.equippable import Equippable
     from components.fighter import Fighter
     from components.inventory import Inventory
     from components.level import Level
@@ -95,6 +97,7 @@ class Actor(Entity):
         color: Tuple[int, int, int] = (255, 255, 255),
         name: str = "<Unnamed>",
         ai_cls: Type[BaseAI],
+        equipment: Equipment,
         fighter: Fighter,
         inventory: Inventory,
         level: Level,
@@ -110,6 +113,9 @@ class Actor(Entity):
         )
 
         self.ai: Optional[BaseAI] = ai_cls(self)
+
+        self.equipment: Equipment = equipment
+        self.equipment.parent = self
 
         self.fighter = fighter
         self.fighter.parent = self
@@ -136,7 +142,8 @@ class Item(Entity):
         char: str = "?",
         color: Tuple[int, int, int] = (255,255,255),
         name: str = "<Unnamed>",
-        consumable: Consumable,
+        consumable: Optional[Consumable] = None,
+        equippable: Optional[Equippable] = None,
     ):
         super().__init__(
             x=x,
@@ -147,5 +154,11 @@ class Item(Entity):
             blocks_movement=False,
             render_order=RenderOrder.ITEM,
         )
-        self.consumable=consumable
-        self.consumable.parent = self
+
+        self.consumable = consumable
+        if self.consumable:
+            self.consumable.parent=self
+
+        self.equippable = equippable
+        if self.equippable:
+            self.equippable.parent = self
